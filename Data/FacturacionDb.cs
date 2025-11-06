d4c1daeb102c5ceff626f02c744d94fa768eab57
yaelito12 <yaelacostatorres@gmail.com>
2025-11-06 13:56:55 -0700

    AGREGAR ENTIDADES EN BASE DE DATOS

--- Data/FacturacionDb.cs
+++ Data/FacturacionDb.cs
@@ -1,6 +1,31 @@
-﻿namespace Facturacion.Data
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Facturacion.Data;
+
+public class FacturacionDbContext : DbContext
 {
-    public class FacturacionDb
+    public FacturacionDbContext(DbContextOptions<FacturacionDbContext> options)
+        : base(options)
     {
     }
+
+    public DbSet<Factura> Facturas { get; set; }
+    public DbSet<Articulo> Articulos { get; set; }
+}
+
+public class Factura
+{
+    public int Id { get; set; }
+    public DateTime Fecha { get; set; }
+    public string NombreCliente { get; set; } = string.Empty;
+    public List<Articulo> Articulos { get; set; } = new();
+    public decimal Total => Articulos?.Sum(a => a.Precio) ?? 0;
+}
+
+public class Articulo
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; } = string.Empty;
+    public decimal Precio { get; set; }
+    public int FacturaId { get; set; }
 }
