38f22a3442bc8737a208887463b4f3dea6a0d424
yaelito12 <yaelacostatorres@gmail.com>
2025-11-25 13:32:54 -0700

    Modificacion de servicio archivoService, coloque contraseña

--- Services/ArchivoService.cs
+++ Services/ArchivoService.cs
@@ -2,5 +2,12 @@
 {
     public class ArchivoService
     {
+      
+        private const string CONTRASEÑA_ARCHIVO = "archivados";
+
+        public bool ValidarContraseña(string contraseña)
+        {
+            return contraseña == CONTRASEÑA_ARCHIVO;
+        }
     }
 }
